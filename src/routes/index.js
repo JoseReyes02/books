bf2c2d6ac65341c6311e9a3e225397a5ef4e73e2
JoseReyes02 <joseeladio29.jer@gmail.com>
2025-08-06 13:03:06 -0400

    probando

--- src/routes/index.js
+++ src/routes/index.js
@@ -23,53 +23,7 @@ router.get('/', async (req, res) => {
   res.render('index',{publicacion});
 });
 
-router.get('/success', async (req, res) => {
-      const code = req.query.code
 
-    try {
-        const { tokens } = await client.getToken(code)
-    
-        const ticket = await client.verifyIdToken({
-            idToken: tokens.id_token,
-            audience: process.env.GOOGLE_CLIENT_ID
-        })
-    
-        const payload = ticket.getPayload()
-    
-        const nombre = payload.name
-        const email = payload.email
-        const photo = payload.picture
-        const estado = 'activo'
-    
-        // Buscar usuario en la base de datos
-        let user = await User.findOne({ email: email })
-    
-        if (!user) {
-            // Si no existe, crear usuario nuevo
-            const totalUsers = await User.countDocuments({ estado: 'activo' })
-            const secuencia = totalUsers + 1
-    
-            user = new User({ nombre, email, photo, estado, secuencia })
-            await user.save()
-    
-        }
-    
-        // Login con passport (esto serializa el user)
-        req.login(user, (err) => {
-            if (err) {
-                console.log(err)
-                return res.redirect('/users/signin')
-            }
-    
-             const dounload = true
-            res.render('success',{dounload}) 
-        })
-    } catch (error) {
-        console.log(error)
-        res.render('/users/signin',{error})
-    }
-  res.render('success');
-});
 
 
 router.post('/admin/register', async (req, res) => {
